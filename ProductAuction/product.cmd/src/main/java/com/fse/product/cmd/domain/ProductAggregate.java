676bead915eac6c2242f7873f359d38661880f56
unknown <cogjava265@iiht.tech>
2022-05-18 15:53:29 +0530

    commiting the code

---
+++ ProductAuction/product.cmd/src/main/java/com/fse/product/cmd/domain/ProductAggregate.java
@@ -0,0 +1,53 @@
+package com.fse.product.cmd.domain;
+
+import com.fse.cqrs.core.domain.AggregateRoot;
+import com.fse.product.cmd.api.commands.AddProductCommand;
+import com.fse.product.common.dto.ProductCategory;
+import com.fse.product.common.events.ProductOpenedEvent;
+
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@NoArgsConstructor
+public class ProductAggregate extends AggregateRoot {
+    private Boolean active;
+    private double balance;
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public ProductAggregate(AddProductCommand command) {
+
+        boolean futureOrNot = getFutureOrNot(command.getBidEndDate());
+        ProductCategory cat = ProductCategory.valueOf(command.getCategory());
+        if(futureOrNot) {
+            raiseEvent(ProductOpenedEvent.builder()
+                    .productId(command.getProductId())
+                    .productName(command.getProductName())
+                    .category(cat.getDisplayName())
+                    .shortDescription(command.getShortDescription())
+                    .startingPrice(command.getStartingPrice())
+                    .bidEndDate(command.getBidEndDate())
+                    .description(command.getDescription())
+                    .build());
+        }
+    }
+
+    private static boolean getFutureOrNot(Timestamp ts)
+    {
+        if(ts.after(new Timestamp(System.currentTimeMillis())))
+            return true;
+        else
+            return false;
+    }
+   /* public void apply(ProductOpenedEvent event) {
+        this.id = event.getId();
+        this.active = true;
+       // this. = event.getCategory();
+    }*/
+
+
+}
---
+++ cqrs/cqrs.core/src/main/java/com/fse/cqrs/core/infrastructure/EventStore.java
@@ -0,0 +1,10 @@
+package com.fse.cqrs.core.infrastructure;
+
+import com.fse.cqrs.core.events.BaseEvent;
+
+import java.util.List;
+
+public interface EventStore {
+    void saveEvents(String aggregateId, Iterable<BaseEvent> events, int expectedVersion);
+
+}
